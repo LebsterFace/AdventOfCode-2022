867b8ca2e4dd702ce164a40ba57c70b818e0a81c
LebsterFace <samsalinsky@gmail.com>
2022-12-24 05:04:15 +0000

    Day 23: Store coordinates as numbers

--- solutions/day23/both.ts
+++ solutions/day23/both.ts
@@ -4,27 +4,32 @@ const input = readFileSync("./input.txt", "utf-8")
 	.replaceAll("\r", "")
 	.split("\n");
 
-type PositionStr = `${number},${number}`;
-type Elf = PositionStr;
+const hashPosition = (x: number, y: number) => ((x + 2**7) << 8) | (y + 2**7);
+const decodePosition = (position: number) => {
+	return [
+		((position & 0xFF00) >> 8) - 2**7,
+		(position & 0x00FF) - 2**7
+	];
+};
 
-let elves = new Set<Elf>;
+let elves = new Set<number>;
 for (let y = 0; y < input.length; y++) {
 	for (let x = 0; x < input[y].length; x++) {
 		if (input[y][x] === '#')
-			elves.add(`${x},${y}`,);
+			elves.add(hashPosition(x, y));
 	}
 }
 
 const getNeighbours = (x: number, y: number) => {
 	return {
-		NW: `${x - 1},${y - 1}` as const,
-		N: `${x},${y - 1}` as const,
-		NE: `${x + 1},${y - 1}` as const,
-		W: `${x - 1},${y}` as const,
-		E: `${x + 1},${y}` as const,
-		SW: `${x - 1},${y + 1}` as const,
-		S: `${x},${y + 1}` as const,
-		SE: `${x + 1},${y + 1}` as const,
+		NW: hashPosition(x - 1, y - 1),
+		N: hashPosition(x, y - 1),
+		NE: hashPosition(x + 1, y - 1),
+		W: hashPosition(x - 1, y),
+		E: hashPosition(x + 1, y),
+		SW: hashPosition(x - 1, y + 1),
+		S: hashPosition(x, y + 1),
+		SE: hashPosition(x + 1, y + 1),
 	};
 };
 
@@ -36,8 +41,8 @@ const checks: [keyof Neighbours, Array<keyof Neighbours>][] = [
 	['E', ['E', 'NE', 'SE']]
 ];
 
-const getProposedPosition = (elf: Elf): PositionStr => {
-	const [x, y] = elf.split(",").map(Number);
+const getProposedPosition = (elf: number): number => {
+	const [x, y] = decodePosition(elf);
 	const neighbours = getNeighbours(x, y);
 
 	if (Object.values(neighbours).every(b => !elves.has(b)))
@@ -53,7 +58,7 @@ const getProposedPosition = (elf: Elf): PositionStr => {
 };
 
 const round = () => {
-	const proposals = new Map<PositionStr, Elf[]>;
+	const proposals = new Map<number, number[]>;
 
 	// first half of the round
 	for (const elf of elves) {
@@ -63,7 +68,7 @@ const round = () => {
 	}
 
 	// second half of the round
-	const newElves = new Set<Elf>;
+	const newElves = new Set<number>;
 	for (const [P, elves] of proposals) {
 		if (elves.length === 1) {
 			// move elf to P
@@ -83,7 +88,7 @@ let i = 1;
 while (round() || i <= 10) {
 	if (i === 10) {
 		// Get the bounds
-		const elfPositions = Array.from(elves, e => e.split(",").map(Number) as [number, number]);
+		const elfPositions = Array.from(elves, decodePosition);
 		let minX = Infinity, minY = Infinity;
 		let maxX = -Infinity, maxY = -Infinity;
 		for (const [x, y] of elfPositions) {
